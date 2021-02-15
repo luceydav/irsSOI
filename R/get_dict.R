6cce04fde595a051f2e78bf42a1a5f4f10d0dfbe
luceydav <dnl2001@stern.nyu.edu>
2021-02-15 11:12:57 -0500

    add get_dict

---
+++ R/get_dict.R
@@ -0,0 +1,68 @@
+
+#' Import NBER IRS Annual Data Dictionary
+#'
+#' @description
+#' Given a year, scrape NBER dictionary and return a data.table
+#' (https://www.nber.org/research/data/individual-income-tax-statistics-zip-code-data-soi)
+#'
+#' @param year int year between 2005-2016
+#'
+#' @examples
+#' \dontrun{
+#' dict <- get_dict(2016)}
+#'
+#' @importFrom stats setNames
+#'
+#' @export
+get_dict <- function(year) {
+
+  # Set URL based on year
+  url <-
+    glue::glue('https://data.nber.org/tax-stats/zipcode/{year}/desc/zipcode{year}/desc.txt')
+
+  # Scrape dictionary
+  txt <- xml2::read_html(url) %>%
+    rvest::html_node("body > p") %>%
+    rvest::html_text()
+  fil <- tempfile(fileext = ".data")
+  cat(txt, file = fil,
+      sep = "\n")
+  d <- readLines(fil, n = -1L)
+  unlink(fil)
+
+  # Filter needed rows
+  d1 <- d[(stringr::str_which(d, "^state:") - 1):(stringr::str_which(d, "obs:") -2)]
+  d1 <- d1[stringr::str_detect(d1, "")]
+  col_2 <- d1[stringr::str_detect(d1, "^\\s")]
+  col_2 <- stringr::str_remove(col_2, "^\\s*")
+  col_2 <- col_2[stringr::str_detect(col_2, "^[1-9]")]
+
+  # Build lists of description bullets
+  row_list <- list()
+  row_lists <- list()
+  for (i in seq_along(col_2) ) {
+    if ( stringr::str_detect(col_2[i], "^1") ){
+      row_list <- append(row_list, list(col_2[i]))
+      if (i < length(col_2) - 1) {
+        j <- i + 1
+        while( !stringr::str_detect(col_2[j], "^1") ) {
+          row_list <- append(row_list, list(col_2[j]))
+          if (j < length(col_2) - 1) { j <- j + 1 }
+          else { break }
+        }
+      }
+      row_lists <- append(row_lists, list(row_list))
+      i <- j
+      row_list <- list()
+      if (i >= length(col_2)) break
+    }
+  }
+
+  # Extract names
+  names <- d1[stringr::str_detect(d1, "^[a-z]")]
+
+  # Compile in data.table
+  dt <- data.table::data.table(names, row_lists)
+
+  return(dt)
+}
