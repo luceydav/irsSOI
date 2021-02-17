dd9834ff7e8f57dc20e4d87ab83cab6f134edd09
luceydav <dnl2001@stern.nyu.edu>
2021-02-16 19:43:50 -0500

    fix field_lookup

--- R/field_lookup.R
+++ R/field_lookup.R
@@ -15,11 +15,13 @@
 #' \dontrun{library(data.table)
 #' field_lookup("2016", "a00100")}
 #'
+#' @importFrom utils data
+#'
 #' @export
 field_lookup <- function(select_year = "2016", name = "a00100") {
+
   if (!is.character(select_year) ) {
     select_year <- as.character(select_year)
   }
-  data("data_dict")
   unlist(data_dict[year == select_year & names == name]$row_lists)
 }
