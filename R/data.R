bb101619c63b60c82cff3656c68ae2a53743143b
David Lucey <dnl2001@stern.nyu.edu>
2023-01-22 08:22:55 -0500

    udpdate and fix data docs

--- R/data.R
+++ R/data.R
@@ -26,8 +26,11 @@
 #'   \item{a02300}{Unemployment Comp, num}
 #'   \item{n02300}{Returns with Unemployment Comp, num}
 #'   \item{a59660}{Earned Income Credit Amount, num}
+#'   \item{n59660}{Returns with earned income credit, num}
 #'   \item{a09750}{Healthcare Individual Resp. Payment, num}
+#'   \item{n09750}{Number of returns with health care, num}
 #'   \item{a04470}{Total Itemized Deductions Amount, num}
+#'   \item{a02500}{Taxable Social Security benefits, num}
 #'   \item{n02500}{Returns with Soc Security, num}
 #'   \item{a18425}{State & Local Taxes Amount, num}
 #'   \item{n18425}{Returns with State & Local Taxes, num}
@@ -41,6 +44,20 @@
 #'   \item{n00600}{Returns with Ordinary Dividends, num}
 #'   \item{a00300}{Taxable Interest Amount, num}
 #'   \item{n00300}{Returns with Taxable Interest, num}
+#'   \item{a00900}{Business or professional net income, num}
+#'   \item{n00900}{Returns with Business or professional, num}
+#'   \item{a19700}{Contributions amount, num}
+#'   \item{n19700}{Returns with Contributions, num}
+#'   \item{a26270}{Partnership/S-corp net income, num}
+#'   \item{n26270}{Returns Partnership/S-corp, num}
+#'   \item{a01700}{Taxable pensions and annuities, num}
+#'   \item{n01700}{Returns with Taxable pensions and annuities, num}
+#'   \item{a09600}{Alternative minimum tax amount, num}
+#'   \item{n09600}{Returns with Alternative minimum tax amount, num}
+#'   \item{a11000}{Earned Income Credit amount, num}
+#'   \item{n11000}{Returns with Earned Income Credit amount, num}
+#'   \item{a85300}{Net investment income tax amount, num}
+#'   \item{n85300}{Returns with Net investment income tax, num}
 #' }
 #' @source \url{https://www.nber.org/research/data/individual-income-tax-statistics-zip-code-data-soi/}
 #'
